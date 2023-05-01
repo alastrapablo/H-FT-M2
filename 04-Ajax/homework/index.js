510b14b9a949c72390f47a879b9ffcac4d80ecda
Pablo Alastra <alastra.pablo@gmail.com>
2023-05-01 15:37:05 -0300

    04

---
+++ 04-Ajax/homework/index.js
@@ -0,0 +1,59 @@
+// const { log } = require("@11ty/eleventy/src/EleventyErrorHandler");
+
+/* 1
+Utiliza el evento click en un boton para hacer que al hacer click en el mismo
+ aparezca en el DOM una lista con todos los amigos que el servidor nos devolvera 
+ al hacer un GET a la ruta http://localhost:5000/amigos
+*/
+
+let URL = 'http://localhost:5000/amigos'
+
+let showFriends = () => {
+    document.querySelector('#lista').innerHTML = ''
+    $.get(`${URL}`, (friends) => {
+        // console.log(friends);
+        friends.forEach(friend => {
+            let li = document.createElement('li')
+            li.innerHTML = `${friend.name}`
+            document.querySelector('#lista').appendChild(li)
+            // console.log(li);
+
+            //JQUERRY
+            // $('#lista').append(`<li id="${friend.id}">${friend.name}</li>`);
+        });
+    });
+}
+
+$('#boton').click(showFriends)
+
+/* 2
+Un campo de busqueda (input) que reciba el id de un amigo y un boton "buscar".
+ Al hacer click en el boton, buscaremos el amigo que tiene ese id en el servidor, 
+ y lo mostraremos en el DOM. Para conseguir los datos de un amigo en particular 
+ del servidor, puedes hacer un GET nuestro servidor concatenando el id del amigo 
+ que queremos encontrar, Por ej: http://localhost:5000/amigos/1 le pediria al 
+ servidor el amigo con id = 1
+*/
+
+$('#search').click(() => {
+
+    let id = $('#input').val()
+    console.log(id);
+
+    if (id) {
+        $.get(`${URL}/${id}`, (friend) => {
+            // console.log(friend);
+            let span = document.querySelector('#amigo')
+            span.innerHTML = `${friend.name}`
+        });
+    }
+})
+
+/* 3
+Un input que reciba el id de un amigo y un boton "borrar". Al hacer click en
+ el boton, borraremos el amigo del servidor haciendo un DELETE a nuestro servidor,
+ concatenando el id del usuario que queremos borrar. 
+ Por ej: http://localhost:5000/amigos/2 le pediria al servidor el amigo con id = 2
+*/
+
+
